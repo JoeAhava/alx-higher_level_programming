4a6a0abc6491e975fd4a23ca9032b69f19fbefd9
Yohannes Terfa <lightshadow011@gmail.com>
2021-09-07 23:14:55 +0000

    task 14-16 refix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    copy = ''
+    copy = ""
     count = 0
-    for s in str:
+    for i in str:
+        if count is n:
+            continue
+        else:
+            copy += i
         count += 1
-    if count < n:
-        copy = str
-    else:
-        for i in str:
-            if i == str[n]:
-                continue
-            else:
-                copy += i
-
     return copy
