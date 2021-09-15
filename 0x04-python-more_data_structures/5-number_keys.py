24da2ee0f7d7c9cff2cb3f09b7f2f1446c80c8c7
Yohannes Terfa <lightshadow011@gmail.com>
2021-09-15 18:04:23 +0000

    task 5 done

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    A function to get number of keys in a dict
+'''
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
+
+
+'''
+if not __name__ == "__main__":
+    // Execute a test
+'''
