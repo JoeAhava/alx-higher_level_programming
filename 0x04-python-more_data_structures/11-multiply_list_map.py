75d23203d91a66895a76d13c9a27b9f9b616c6ff
Yohannes Terfa <lightshadow011@gmail.com>
2021-09-15 19:15:50 +0000

    task 11 done

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-'''
-    A function to multiply by two
-    every element in a list
-'''
-
-
 def multiply_list_map(my_list=[],number=0):
     return list(map(lambda x: x * number, my_list))
-
-
-'''
-if not __name__ == "__main__":
-    // Execute a test
-'''
