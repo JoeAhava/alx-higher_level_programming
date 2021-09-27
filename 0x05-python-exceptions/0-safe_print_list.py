1203733ba3f7ddef4529ed2de2b16823ec834ae2
Yohannes Terfa <lightshadow011@gmail.com>
2021-09-27 19:11:37 +0000

    task 0 done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,15 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     try:
-        while counter <= x: 
+        while counter <= x:
             print(my_list[counter], end='')
             counter += 1
 
-        print('')
     except IndexError:
         pass
 
+    print("", end='\n')
+
 
 if __name__ == "__main__":
-    safe_print_list(my_list=[1,2,3,4,5], x=7)
+    safe_print_list(my_list=[1, 2, 3, 4, 5], x=7)
