5f15881d558599815c7ed9bf71a1e406f7940954
Yohannes Terfa <lightshadow011@gmail.com>
2021-09-27 19:31:51 +0000

    task 1 done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except Exception:
         return False
 
 if __name__ == "__main__":
