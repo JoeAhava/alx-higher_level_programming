bc9a1ce1ad3cb46c5676eb931406015e47d52a3c
Yohannes Terfa <lightshadow011@gmail.com>
2021-11-17 10:35:03 +0000

    task 15 refix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count and display
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
