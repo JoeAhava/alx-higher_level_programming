28406e8b578b90298ca713b4efc5c96e46059200
Yohannes Terfa <lightshadow011@gmail.com>
2021-12-21 09:38:15 +0000

    refix select states argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-'''
-Lists all states from a database containing states data
-USAGE:
-    ./0-select_states.py <USER_NAME> mysql <PASSWORD> <DATABASE_NAME>
-'''
+
+#   Lists all states from a database containing states data
+#   USAGE:
+#    ./0-select_states.py <USER_NAME> mysql <PASSWORD> <DATABASE_NAME>
+
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
-            db=sys.argv[4],
+            db=sys.argv[3],
             user=sys.argv[1],
-            password=sys.argv[3])
+            password=sys.argv[2])
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY id ASC')
     [print(state) for state in cursor.fetchall()]
