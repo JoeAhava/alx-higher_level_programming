f0538a91f73152ab9bd89c008363dc585d9a9c4b
JoeAhava <45706881+JoeAhava@users.noreply.github.com>
2022-01-10 16:40:27 +0300

    q0_update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in URL and sends a request to that URL and displays size of the body for the response
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
